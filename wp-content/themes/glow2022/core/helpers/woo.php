54b78197f1bf2cd80dc6dbae9f128f31ee904e9d
glowmedia <steve@glowmedia.co.uk>
2022-12-13 14:22:38 +0000

    remove woo theme support

--- wp-content/themes/glow2022/core/helpers/woo.php
+++ wp-content/themes/glow2022/core/helpers/woo.php
@@ -4,7 +4,7 @@ function mytheme_add_woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
 
-add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+//add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
